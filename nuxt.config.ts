94915d6e0f93269e92f03a789cab6f3ebc5a524b
MahardiikaKD <dikamahar884@gmail.com>
2023-08-09 19:43:19 +0700

    heroo banner

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: "Nuxt Template"
+    },
+  },
   css: ['~/assets/css/main.css'],
   modules: ['@nuxtjs/tailwindcss'],
   postcss: {
